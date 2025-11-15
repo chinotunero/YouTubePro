f819f4b00d195b4a27ea48166babbb49c1a504ff
chinotunero <133407505+chinotunero@users.noreply.github.com>
2025-11-15 03:29:53 +0100

    Refactor gradlew.bat for improved execution flow

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,17 @@
 @echo off
-"%JAVA_HOME%in\java.exe" -cp "gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
+
+if exist "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" (
+    set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+) else (
+    echo ERROR: gradle-wrapper.jar not found
+    exit /b 1
+)
+
+"%JAVA_HOME%\bin\java.exe" %DEFAULT_JVM_OPTS% -cp %CLASSPATH% org.gradle.wrapper.GradleWrapperMain %*
